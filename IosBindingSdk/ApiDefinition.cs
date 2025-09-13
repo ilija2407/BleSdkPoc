1ca9efae9a4db3279f519e1924e73ef8b62bd76d
Ilija Rushkovski <ilijarushkovski@gmail.com>
2025-09-13 23:10:21 +0200

    ios sdk

---
+++ IosBindingSdk/ApiDefinition.cs
@@ -0,0 +1,35 @@
+using System;
+using Foundation;
+using ObjCRuntime;
+
+namespace IgloohomeProxy
+{
+    // Objective-C class: IgloohomeSwitfProxy
+    [BaseType(typeof(NSObject), Name = "IgloohomeSwitfProxy")]
+    interface IgloohomeSwitfProxy
+    {
+        [Export("init")]
+        IntPtr Constructor();
+
+        // -(void)scanOnSuccess:(void (^)(IglooLockProxy *lock))onSuccess onFail:(void (^)(NSString *message))onFail;
+        [Export("scanOnSuccess:onFail:")]
+        void ScanOnSuccess(Action<IglooLockProxy> onSuccess, Action<string> onFail);
+
+        // -(void)stopScan;
+        [Export("stopScan")]
+        void StopScan();
+    }
+
+    // Objective-C class: IglooLockProxy
+    [BaseType(typeof(NSObject), Name = "IglooLockProxy")]
+    interface IglooLockProxy
+    {
+        // @property (readonly, copy, nonatomic) NSString *name;
+        [Export("name")]
+        string Name { get; }
+
+        // @property (readonly, copy, nonatomic) NSString *identifier;
+        [Export("identifier")]
+        string Identifier { get; }
+    }
+}
